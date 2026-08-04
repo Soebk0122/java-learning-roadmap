49030a04c3fcc03fbd64273b24efb5045f0e6f60
Soebk0122 <weisui122812@gmail.com>
2026-08-04 22:52:47 +0800

    feat: Day9 完成Student类定义与对象实例化，初探面向对象

---
+++ day09_class/src/com/example/test/Student1.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+public class Student1 {
+    String name;
+    int age;
+    char sex;
+    public Student1(){
+
+    }
+    public Student1(String name,int age,char sex){
+        this.name=name;
+        this.age=age;
+        this.sex=sex;
+    }
+}
