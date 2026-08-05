a26649b469ca9e7409d26f234f6615548dfc15ac
Soebk0122 <weisui122812@gmail.com>
2026-08-05 22:50:12 +0800

    feat: Day10 完成JavaBean规范类与static关键字学习

--- day10_fengzhuang/src/com/example/day10/javabean/Demo2.java
+++ day10_fengzhuang/src/com/example/day10/javabean/Demo2.java
@@ -1,4 +1,22 @@
 package com.example.day10.javabean;
 
 public class Demo2 {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.setName("张三"); ;
+        s1.setChinese(80.0);
+        s1.setMath(90.0);
+        System.out.println(s1.getName());
+        System.out.println(s1.getChinese());
+        System.out.println(s1.getMath());
+
+        Student s2=new Student("李四", 75.0, 80.0);
+        System.out.println(s2.getName());
+        System.out.println(s2.getChinese());
+        System.out.println(s2.getMath());
+
+        Studentservice ss=new Studentservice(s2);
+        ss.print1();
+        ss.print2();
+    }
 }
--- day10_fengzhuang/src/com/example/day10/javabean/Studentservice.java
+++ day10_fengzhuang/src/com/example/day10/javabean/Studentservice.java
@@ -1,4 +1,14 @@
 package com.example.day10.javabean;
 
 public class Studentservice {
+    private Student s;
+    public Studentservice(Student s){
+        this.s = s;
+    }
+    public void print1(){
+        System.out.println(s.getName() + "的总成绩：" + (s.getChinese() + s.getMath()));
+    }
+    public void print2(){
+        System.out.println(s.getName() + "的平均成绩：" + (s.getChinese() + s.getMath()) / 2);
+    }
 }
