85a8ce901e955aa832bba2dada31ddc45e455a7b
Soebk0122 <weisui122812@gmail.com>
2026-08-06 22:35:36 +0800

    feat: Day11 完成static剩余学习 进行综合实战

--- day11_static/src/com/example/day11/Demo1.java
+++ day11_static/src/com/example/day11/Demo1.java
@@ -2,6 +2,13 @@ package com.example.day11;
 
 public class Demo1 {
     public static void main(String[] args) {
+        Movie[] movies=new Movie[6];
+        movies[0]=new Movie(1,"唐顿庄园",9.5,"唐顿·戴维森");
+        movies[1]=new Movie(2,"飞驰人生",9.8,"沈腾");
+        movies[2]=new Movie(3,"战狼1",9.6,"吴京");
+        movies[3]=new Movie(4,"流浪地球",9.4,"吴京");
+        movies[4]=new Movie(5,"功夫女足",9.1,"张艺兴");
+        movies[5]=new Movie(6,"想你了",9.2,"章若楠");
 
     }
 }
