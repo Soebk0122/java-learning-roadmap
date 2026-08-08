af0a624c9a76110fb9348504d97a8f912510571d
Soebk0122 <weisui122812@gmail.com>
2026-08-08 22:04:50 +0800

    feat: Day12 学习继承

---
+++ day12_jicheng/src/com/example/day12/Test1/Demo1.java
@@ -0,0 +1,13 @@
+package com.example.day12.Test1;
+
+public class Demo1 extends People{
+    private int numeber;
+
+    public int getNumeber() {
+        return numeber;
+    }
+
+    public void setNumeber(int numeber) {
+        this.numeber = numeber;
+    }
+}
---
+++ day12_jicheng/src/com/example/day12/Test2/Demo1.java
@@ -0,0 +1,11 @@
+package com.example.day12.Test2;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Fu fu = new Fu();
+        fu.publicshow();
+        fu.show();
+//        fu.privateShow();
+        fu.protectedShow();
+    }
+}
