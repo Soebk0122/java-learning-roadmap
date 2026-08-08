ad3c009707b8df312fc61f1156a03533ff3d87c9
Soebk0122 <weisui122812@gmail.com>
2026-08-08 22:06:14 +0800

    feat: Day12 学习继承

---
+++ day12_jicheng/src/com/example/day12/Test3/Test1.java
@@ -0,0 +1,13 @@
+package com.example.day12.Test3;
+
+import com.example.day12.Test2.Fu;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Fu fu = new Fu();
+        fu.publicshow();
+//        fu.show();
+//        fu.privateShow();
+//        fu.protectedShow();
+    }
+}
---
+++ day12_jicheng/src/com/example/day12/Test3/Test2.java
@@ -0,0 +1,10 @@
+package com.example.day12.Test3;
+
+import com.example.day12.Test2.Fu;
+
+public class Test2 extends Fu {
+    public void print1(){
+        protectedShow();
+        publicshow();
+    }
+}
