c0dbaf285edb2bab408ea951d75961e68001a2f5
Soebk0122 <weisui122812@gmail.com>
2026-09-12 22:25:14 +0800

    feat: 学习认识异常，异常的作用 处理异常

--- day17_yichang/src/com/example/day17/Demo2.java
+++ day17_yichang/src/com/example/day17/Demo2.java
@@ -1,16 +1,23 @@
 package com.example.day17;
 
 public class Demo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("程序开始");
-        System.out.println(div(10, 0));
+        try {
+            System.out.println(div(10, 0));
+            System.out.println("底层方法执行成功了");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("异常了");
+        }
         System.out.println("程序结束");
     }
 
-    public static int div(int a, int b){
+    public static int div(int a, int b) throws Exception     {
         if (b == 0){
             System.out.println("除数不能为0");
-            throw new RuntimeException("除数不能为0"); // 运行时异常，程序会终止
+//            throw new RuntimeException("除数不能为0");//运行时异常，程序会终止
+            throw new Exception("除数不能为0");
         }
         int result = a / b;
         return result;
---
+++ day17_yichang/src/com/example/day17/Demo3.java
@@ -0,0 +1,5 @@
+package com.example.day17;
+
+public class Demo3 {
+
+}
